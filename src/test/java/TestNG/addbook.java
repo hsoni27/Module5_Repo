896c85e5cbc626c514b6b86cd862f3c548f63151
hsoni <hitesh2375@gmail.com>
2019-05-05 21:44:43 -0400

    chromedriver.exe modified

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -15,7 +15,7 @@ public class addbook {
 	  public void launch() {
 		  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		  driver = new ChromeDriver();
-	 	  driver.get("http://192.168.2.6:8090/addressbook");
+	 	  driver.get("http://192.168.2.14:8090/addressbook");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	  }
